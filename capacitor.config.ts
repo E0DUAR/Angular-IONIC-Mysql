a7de027b46d3e038c263bcc43282dd57abd411b3
Eduardo Mendez <dizu.eduardo@gmail.com>
2023-03-20 17:50:43 -0500

    Creación del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Angular-IONIC-Mysql',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
