f73ecd8da2765886eb73292fa3455e1f76070122
Eduardo Mendez <dizu.eduardo@gmail.com>
2023-03-21 16:51:42 -0500

    Creación del modulo para eliminar registros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'form',
     loadChildren: () => import('./form/form.module').then( m => m.FormPageModule)
   },
+  {
+    path: 'editar',
+    loadChildren: () => import('./editar/editar.module').then( m => m.EditarPageModule)
+  },
 
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+import { UserService } from '../_services/user.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,11 +14,18 @@ export class HomePage {
 
   constructor(private http: HttpClient) {
     //CONSULTAR DE LA BASE DE DATOS
-    this.http
-      .get('http://localhost/proyectos/crud-php-to-webservice/consultados.php')
-      .subscribe((snap) => {
+    this.http.get('http://localhost/proyectos/crud-php-to-webservice/consultados.php').subscribe((snap) => {
         console.log(snap);
         this.listado = snap;
       });
   }
+
+  
+  eliminar(id:string){
+    this.http.get('http://localhost/proyectos/crud-php-to-webservice/eliminar.php?id='+id).subscribe((snap) => {
+      console.log(snap);
+    });
+  }
+
+
 }
